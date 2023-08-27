419c0a55b27347d319ac83257a4e83847c8630f9
SUHONG\topic <topic828@Naver.com>
2023-08-27 17:04:48 +0900

    redux store 등록

--- houssg/src/index.tsx
+++ houssg/src/index.tsx
@@ -1,19 +1,24 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { store } from './store/redux';
+import { Provider } from 'react-redux';
 
-import { basicTheme } from './assets/styles/theme';
 import App from './App';
+import { basicTheme } from './assets/styles/theme';
 import GlobalStyle from './assets/styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
 	<ThemeProvider theme={basicTheme}>
 		{/* <QueryClientProvider client={client}> */}
-		<BrowserRouter>
-			<GlobalStyle />
-			<App />
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<GlobalStyle />
+				<App />
+			</BrowserRouter>
+		</Provider>
 		{/* </QueryClientProvider> */}
 	</ThemeProvider>,
 );
