84e880edcfc485d6f89bbbaf641a25d031e52c9c
suhong99 <topic828@naver.com>
2023-08-30 14:50:08 +0900

    :pencil2: 오타떄문에 transition 적용안되는 것 수정
    오타떄문에 transition 적용안되는 것 수정

--- houssg/src/components/auth/element/AuthModeBtn.tsx
+++ houssg/src/components/auth/element/AuthModeBtn.tsx
@@ -31,9 +31,10 @@ const AuthModeBtnContainer = styled.div`
 
 const HoverText = styled.div`
 	color: ${color.color4};
-	font-size: 0.8;
+	font-size: 0.8rem;
 	font-weight: 600;
 	transition: font-size 0.2s;
+
 	cursor: pointer;
 
 	&:hover {
