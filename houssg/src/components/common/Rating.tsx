9b0cf4b2450157f91b9eb5f5705f91e86ed82053
woojiyee <77909833+woojiyee@users.noreply.github.com>
2023-09-06 15:55:08 +0900

    별점 컴포넌트 코드 정리

--- houssg/src/components/common/Rating.tsx
+++ houssg/src/components/common/Rating.tsx
@@ -1,27 +1,46 @@
 import React from 'react';
+import { useEffect } from 'react';
+// import { useState } from 'react';
 import { Rate } from 'antd';
 import { styled } from 'styled-components';
 
 interface Props {
-	rate: number;
 	readonly?: boolean;
+	rate: number;
+	setRate?: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const roundRate = (rate: number) => {
-	return Math.round(rate);
+const Rating: React.FC<Props> = ({ readonly, rate = 0, setRate }) => {
+	const roundRate = (rate: number) => {
+		return Math.round(rate * 2) / 2;
+	};
+
+	const handleRate = (rate: number) => {
+		if (setRate) {
+			setRate(rate);
+		}
+	};
+
+	useEffect(() => {}, [rate]);
+
+	return (
+		<Wrapper>
+			{readonly ? (
+				<Rate allowHalf disabled={readonly} value={roundRate(rate)} style={{ width: '8.5rem' }} />
+			) : (
+				<Rate allowHalf style={{ width: '8.5rem' }} onChange={(num) => handleRate(num)} />
+			)}
+
+			{rate.toFixed(1)}
+		</Wrapper>
+	);
 };
 
-const Rating: React.FC<Props> = ({ rate, readonly }) => (
-	<Wrapper>
-		<Rate allowHalf disabled={readonly} defaultValue={roundRate(rate)} style={{ width: '8.5rem' }} />
-		{rate.toFixed(1)}
-	</Wrapper>
-);
+export default Rating;
 
 const Wrapper = styled.div`
-	width: 18rem;
+	width: 18vw;
 	display: grid;
-	grid-template-columns: 1fr 1fr;
+	grid-template-columns: 2fr 1fr;
 	align-items: center;
 `;
-export default Rating;
