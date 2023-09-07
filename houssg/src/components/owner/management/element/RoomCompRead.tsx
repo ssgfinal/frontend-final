cf976f56a4439f76cea476878f7afea8a2951680
suhong99 <topic828@naver.com>
2023-09-07 16:52:31 +0900

    :construction: 객실 read및 댓글 러프 디자인
    객실 read및 댓글 러프 디자인

--- houssg/src/components/owner/management/element/RoomCompRead.tsx
+++ houssg/src/components/owner/management/element/RoomCompRead.tsx
@@ -4,15 +4,16 @@ import { styled } from 'styled-components';
 
 const RoomCompRead: React.FC<RoomComp> = ({ room, setIsEditMode }) => {
 	return (
-		<div>
-			{/* swiper? */}
+		<RoomContainer>
 			<RoomImg src={room.room_image} />
-			<div>객실 가격 : {room.room_price}</div>
-			<div>객실 종류 : {room.room_category}</div>
-			<div>객실 갯수 : </div>
-			<div>객실 설명 : {room.room_detail}</div>
+			<div>
+				<div>객실 가격 : {room.room_price}</div>
+				<div>객실 종류 : {room.room_category}</div>
+				<div>객실 갯수 : </div>
+				<div>객실 설명 : {room.room_detail}</div>
+			</div>
 			<button onClick={() => setIsEditMode(true)}>수정하기</button>
-		</div>
+		</RoomContainer>
 	);
 };
 
@@ -20,4 +21,10 @@ export default RoomCompRead;
 
 const RoomImg = styled.img`
 	width: 10vw;
+`;
+
+const RoomContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
 `;
