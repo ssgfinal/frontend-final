a9a5614405c6e1b691cef0c289754a6a8a59966c
SUHONG\topic <topic828@Naver.com>
2023-09-20 01:38:54 +0900

    :sparkles: 라이브러리 추가 +설정중
    라이브러리 추가 +설정중

--- houssg/src/components/common/ImageUploader.tsx
+++ houssg/src/components/common/ImageUploader.tsx
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
 import { useImageConverter } from '../../hooks';
 import { flexCenter } from '../../assets/styles';
+import Cropper, { ReactCropperElement } from 'react-cropper';
+import { createRef, useState } from 'react';
+import 'cropperjs/dist/cropper.css';
 
 const ImageUploader: React.FC = () => {
 	const { imgRef, imgFile, setIncodedImg } = useImageConverter();
+	const cropperRef = createRef<ReactCropperElement>();
+	const [cropData, setCropData] = useState<string>('');
+
+	const getCropData = () => {
+		console.log(cropperRef.current);
+		if (typeof cropperRef.current?.cropper !== 'undefined') {
+			setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+		}
+	};
 
 	return (
 		<UploadContainer>
@@ -12,6 +24,47 @@ const ImageUploader: React.FC = () => {
 				<UploadButton onClick={() => imgRef.current?.click()}>업로드하기</UploadButton>
 			) : (
 				<StyledImg onClick={() => imgRef.current?.click()} src={imgFile ? imgFile : undefined} alt="사업자 이미지" />
+			)}
+			{imgFile && (
+				<>
+					<Cropper
+						ref={cropperRef}
+						src={imgFile}
+						scaleX={1}
+						scaleY={1}
+						style={{ height: 300, width: 300 }} //크롭 크기
+						cropBoxResizable={false}
+						modal={false} // 검은색 모달창 제거
+						background={false} // 점자배경 제거
+						initialAspectRatio={1}
+						center={true}
+						rotatable={true}
+						draggable={true}
+						dragMode="move"
+						scalable={false}
+						zoomable={true}
+						movable={false}
+						preview=".img-preview"
+						viewMode={1}
+						responsive={true} // 반응하도록 (화면 크기 작아질때 리랜더)
+						autoCropArea={1}
+						checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+						guides={true}
+					/>
+					<div>머징</div>
+				</>
+			)}
+
+			{cropData && (
+				<>
+					<h1>
+						<span>Crop</span>
+						<button style={{ float: 'right' }} onClick={getCropData}>
+							Crop Image
+						</button>
+					</h1>
+					<img style={{ width: '100%' }} src={cropData} alt="cropped" />
+				</>
 			)}
 		</UploadContainer>
 	);
@@ -33,6 +86,7 @@ const StyledImg = styled.img`
 
 const UploadContainer = styled.div`
 	${flexCenter}
+	flex-direction: column;
 `;
 
 const UploadButton = styled.div`
--- houssg/src/hooks/useImageConverter.ts
+++ houssg/src/hooks/useImageConverter.ts
@@ -5,7 +5,7 @@ export const useImageConverter = () => {
 	const imgRef = useRef<HTMLInputElement | null>(null);
 	const [imgFile, setImgFile] = useState<string | null>(null);
 	const setIncodedImg = async () => {
-		if (imgRef.current?.files) {
+		if (imgRef.current?.files && imgRef.current.files[0]) {
 			const file = imgRef.current.files[0];
 			const webpfile = await webpImageIncoder(file);
 			setImgFile(webpfile + ''); // unknown type을 string으로 변환
