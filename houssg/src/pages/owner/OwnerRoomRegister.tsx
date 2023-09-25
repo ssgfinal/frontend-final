601bd493920a56a563fbab291299a13471b6c2ab
suhong99 <topic828@naver.com>
2023-09-25 17:52:33 +0900

    :construction: 제목만 추가
    제목만 추가

--- houssg/src/pages/owner/OwnerRoomRegister.tsx
+++ houssg/src/pages/owner/OwnerRoomRegister.tsx
@@ -1,9 +1,33 @@
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { RegiHeadText } from '../../assets/styles';
 
 const OwnerRoomRegister = () => {
 	const { houseId } = useParams();
 
-	return <div>방하이 {houseId}</div>;
+	return (
+		<RoomRegisterWrap>
+			<RegiHeadText>객실 등록</RegiHeadText>
+			<RegisterInputWrapper>
+				<div>객실종류</div>
+				<div>객실 사진</div>
+				<div>객실 서비스</div>
+				<div>방 개수</div>
+				<div>객실 가격</div>
+			</RegisterInputWrapper>
+			<br />
+			<div>{houseId}</div>
+		</RoomRegisterWrap>
+	);
 };
 
 export default OwnerRoomRegister;
+
+const RoomRegisterWrap = styled.div`
+	margin: 1rem auto;
+	max-width: 700px;
+`;
+
+const RegisterInputWrapper = styled.div`
+	margin-top: 0.5rem;
+`;
