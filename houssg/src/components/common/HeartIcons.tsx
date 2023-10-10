093470dbe6d93746bcec97515cf59e8358abc551
woojiyee <77909833+woojiyee@users.noreply.github.com>
2023-10-10 14:11:21 +0900

    :lipstick: props에서 찜 여부 받았던 코드 -> 찜 컴포넌트에서 api 요청으로 백으로부터 찜 여부 받아오는 코드로 수정 state에 아이콘을 담았던 코드 -> state는 좋아요 여부를 담고 아이콘은 좋아요 여부에 따라 img src 분기처리해서 띄워주는 코드로 수정 좋아요 컴포넌트가 언마운트되는 시점에서 초기 좋아요 상태와 현재 좋아요 상태가 다를 때만  변경된 좋아요 여부에 따라 api 통신 컴포넌트 css - 크기를 해당 컴포넌트에서 고정시키는 코드 -> 부모 컴포넌트에 맞게 변하게 하여 부모 컴포넌트에서 하트 이모티콘 크기 조절 가능하게 했음

--- houssg/src/components/common/HeartIcons.tsx
+++ houssg/src/components/common/HeartIcons.tsx
@@ -1,16 +1,14 @@
 import { styled } from 'styled-components';
 import { HeartIcon, FullHeartIcon } from '../../assets/icons/index';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { isLoginFunc } from '../../utils';
 import { useAppDispatch } from '../../hooks';
 import { openModal } from '../../store/redux/modalSlice';
+import api from '../../api/api';
+import { userUrl } from '../../assets/constant';
 
-interface Props {
-	favorite: boolean;
-}
-
-const HeartIcons: React.FC<Props> = ({ favorite }) => {
-	const [heartSrc, setHeartSrc] = useState<string>(favorite ? FullHeartIcon : HeartIcon);
+const HeartIcons = () => {
+	const [isLike, setIsLike] = useState<boolean>(false);
 
 	const dispatch = useAppDispatch();
 
@@ -20,11 +18,29 @@ const HeartIcons: React.FC<Props> = ({ favorite }) => {
 			const modalSize = window.innerWidth >= 1000 ? 500 : 400;
 			dispatch(openModal({ modalComponent: 'auth', modalSize: modalSize }));
 		} else {
-			setHeartSrc((prevHeartSrc) => (prevHeartSrc === HeartIcon ? FullHeartIcon : HeartIcon));
+			setIsLike(!isLike);
 		}
 	};
 
-	return <HeartImg onClick={HeartClick} src={heartSrc} />;
+	useEffect(() => {
+		let initLike: boolean;
+		// TODO: 백 api 만들어지면 연결하기
+		// api.get(userUrl.like).then(({ data }) => {
+		// 	initLike = data.isLike;
+		// 	setIsLike(initLike);
+		// }); // initLike 이렇게 let으로 해보고 잘 안 먹히면 initLike를 state로 관리하기
+		return () => {
+			if (initLike !== isLike) {
+				if (isLike) {
+					api.post(userUrl.addLike);
+				} else {
+					api.delete(userUrl.like);
+				}
+			}
+		};
+	}, []);
+
+	return <HeartImg onClick={HeartClick} src={isLike ? FullHeartIcon : HeartIcon} />;
 };
 
 export default HeartIcons;
@@ -32,5 +48,4 @@ export default HeartIcons;
 const HeartImg = styled.img`
 	width: 100%;
 	height: 100%;
-	/* width: 1rem; */
 `;
