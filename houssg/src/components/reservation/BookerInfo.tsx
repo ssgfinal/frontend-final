84b585206942c3f9becc24b8a9fd761329474a21
woojiyee <77909833+woojiyee@users.noreply.github.com>
2023-10-15 15:54:38 +0900

    :lipstick: 예약자 정보 sessionStorage에서 받아옴

--- houssg/src/components/reservation/BookerInfo.tsx
+++ houssg/src/components/reservation/BookerInfo.tsx
@@ -1,14 +1,6 @@
 import { ReservationCommonBox, UserReservationTitle, UserReservationLeft } from '../../assets/styles';
 
 export const BookerInfo = () => {
-	// const reservation = {
-	// 	id: 1,
-	// 	night: 2,
-	// 	price: 64000,
-	// 	userNickName: '김도로뇽',
-	// 	userPhone: '01012345678',
-	// };
-
 	const userNickName = sessionStorage.getItem('nickname');
 	const userPhone = sessionStorage.getItem('phone');
 
