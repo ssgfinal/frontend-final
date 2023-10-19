6071f5835b414d1b79fa1921b06b2b0ee9af4dcd
woojiyee <77909833+woojiyee@users.noreply.github.com>
2023-10-19 19:05:37 +0900

    :lipstick: 인터페이스 업데이트

--- houssg/src/types/index.ts
+++ houssg/src/types/index.ts
@@ -1,7 +1,7 @@
 // type 지정을 위함 type 혹은 interface는 이 폴더에서 관리합니다
 
 import { AuthProps, AuthInputType, AuthModeType, AuthPropsWithState, IdFindingType, SmsParameter, NewPwProps, FindPwPermitProps } from './auth';
-import { ReservationDetailType, ReservationsType, MyReservation, BookableRoomCnt, Schedule, SelectedReservationType } from './reservation';
+import { ReservationDetailType, ReservationsType, BookableRoomCnt, Schedule, SelectedReservationType } from './reservation';
 import { ManageNavProps, MyHouseData, MyHouseDataHandleComp, EditMutationType, OwnerHouseReviewType } from './manage';
 import { StyledActiveProps, SetStateToggle, UserType, UserTypeObject, RoomSlideProps, TimerProps, ProcessType } from './common';
 import { RegiStepProps, StepMoverType, FunnelPropsType } from './regi';
@@ -19,7 +19,7 @@ export type { AuthProps, AuthInputType, AuthModeType, AuthPropsWithState, IdFind
 export type { StyledActiveProps, SetStateToggle, UserType, UserTypeObject, RoomSlideProps, TimerProps, ProcessType };
 
 //reservation
-export type { ReservationDetailType, ReservationsType, MyReservation, BookableRoomCnt, Schedule, SelectedReservationType };
+export type { ReservationDetailType, ReservationsType, BookableRoomCnt, Schedule, SelectedReservationType };
 
 //ownerReservation
 
