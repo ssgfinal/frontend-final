6912c896c7c8737d85144f12d264ec0c029e7138
HanJuRi999 <hanjury0901@gmail.com>
2023-10-22 00:31:56 +0900

    :bug: 충동해결
    충동해결

--- houssg/src/components/common/CommonModal.tsx
+++ houssg/src/components/common/CommonModal.tsx
@@ -13,6 +13,7 @@ import CancelReservation from '../reservation/CancelReservation';
 import ReviewWrite from '../reservation/ReviewWrite';
 import { DeclarationReview, DeleteRequestScreen, HouseComment } from '../owner/management/element';
 import { OwnerReservationModal } from '../owner/reservation';
+import KaKaoSignUp from '../auth/KaKaoSignUp';
 import Preview from '../reservation/Preview';
 
 const CommonModal = () => {
@@ -37,6 +38,7 @@ const CommonModal = () => {
 			width={size}
 		>
 			{modalComp === 'auth' && <AuthWrap />}
+			{modalComp === 'kakaoSignUp' && <KaKaoSignUp />}
 			{modalComp === 'editNickName' && <EditNickName />}
 			{modalComp === 'editPhoneNumber' && <EditPhoneNumber />}
 			{modalComp === 'editPassword' && <EditPassword />}
