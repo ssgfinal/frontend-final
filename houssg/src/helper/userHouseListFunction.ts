dc76362b879e32ecff468e3395b6e7b9f762cc8b
HanJuRi999 <hanjury0901@gmail.com>
2023-10-26 00:54:50 +0900

    :lipstick: 메인 배너 및 숙소 검색
    메인 배너 및 숙소 검색

--- houssg/src/helper/userHouseListFunction.ts
+++ houssg/src/helper/userHouseListFunction.ts
@@ -1,9 +1,8 @@
 import { api } from '../api';
 import { userUrl } from '../assets/constant';
 
-const getUserHouseList = async (search: string, type: string, select: string, page: number, { pageParam = 1 }) => {
-	const resp = await api.get(userUrl.houseList, { params: { search, type, select, page, pageParam } });
-
+const getUserHouseList = async (search: string, type: string, select: string, page: number) => {
+	const resp = await api.get(userUrl.houseList, { params: { search, type, select, page } });
 	return resp.data;
 };
 
